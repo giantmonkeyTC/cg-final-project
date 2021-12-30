f72cdcfa92abe25c6a12437c0d20d23149c8c363
giantmonkeyTC <tcgiantmonkey@gmail.com>
2021-12-30 18:13:44 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,30 @@
 const webpack = require("webpack");
+var path = require('path');
+var HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            }
+        ]
 
-plugins: [
-    new webpack.ProvidePlugin({'THREE':'three/build/three'})
-]
+    },
+    devServer: {
+        proxy: { '/api': 'http://localhost:3000' },
+        compress: true,
+        port: 7777
+    },
+    plugins: [
+        new HtmlWebPackPlugin({ title: "threejs" }),
+        new webpack.ProvidePlugin({ 'THREE': 'three/build/three' })
+    ]
+}
