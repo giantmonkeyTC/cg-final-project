369ff3ac187d317eee09c5b5ef844c84ba051538
GNTMKY <tcgiantmonkey@gmail.com>
2022-01-22 14:07:38 +0800

    bird

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three"
-import { BufferAttribute, Color, EventDispatcher, Points } from "three"
+import { BufferAttribute, Color, EventDispatcher, Material, Points } from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import * as CONTROL from "three/examples/jsm/controls/OrbitControls.js"
 import { time, timeStamp } from "console";
@@ -41,6 +41,9 @@ loader.load('/scene.gltf', function (gltf) {
 const canvas = document.createElement('canvas');
 canvas.width = 1;
 canvas.height = 32;
+
+const WIDTH = 32;
+const BIRDS = WIDTH * WIDTH;
 
 const context = canvas.getContext('2d');
 const gradient = context.createLinearGradient(0, 0, 0, 24);
@@ -337,6 +340,198 @@ function seasonDisable() {
     instancedMesh.visible = false;
 }
 
+class BirdGeometry extends THREE.BufferGeometry {
+
+    constructor() {
+
+        super();
+
+        const trianglesPerBird = 3;
+        const triangles = BIRDS * trianglesPerBird;
+        const points = triangles * 3;
+
+        const vertices = new THREE.BufferAttribute(new Float32Array(points * 3), 3);
+        const birdColors = new THREE.BufferAttribute(new Float32Array(points * 3), 3);
+        const references = new THREE.BufferAttribute(new Float32Array(points * 2), 2);
+        const birdVertex = new THREE.BufferAttribute(new Float32Array(points), 1);
+
+        this.setAttribute('position', vertices);
+        this.setAttribute('birdColor', birdColors);
+        this.setAttribute('reference', references);
+        this.setAttribute('birdVertex', birdVertex);
+
+        // this.setAttribute( 'normal', new Float32Array( points * 3 ), 3 );
+
+
+        let v = 0;
+        function verts_push(vert: number, ...restofVert: number[]) {
+
+            for (let i = 0; i < arguments.length; i++) {
+
+                (vertices.array as Float32Array)[v++] = arguments[i];
+
+            }
+
+        }
+
+        const wingsSpan = 20;
+
+        for (let f = 0; f < BIRDS; f++) {
+
+            // Body
+
+            verts_push(
+                0, - 0, - 20,
+                0, 4, - 20,
+                0, 0, 30
+            );
+
+            // Wings
+
+            verts_push(
+                0, 0, - 15,
+                - wingsSpan, 0, 0,
+                0, 0, 15
+            );
+
+            verts_push(
+                0, 0, 15,
+                wingsSpan, 0, 0,
+                0, 0, - 15
+            );
+
+        }
+
+        for (let v = 0; v < triangles * 3; v++) {
+
+            const triangleIndex = ~ ~(v / 3);
+            const birdIndex = ~ ~(triangleIndex / trianglesPerBird);
+            const x = (birdIndex % WIDTH) / WIDTH;
+            const y = ~ ~(birdIndex / WIDTH) / WIDTH;
+
+            const c = new THREE.Color(
+                0x444444 +
+                ~ ~(v / 9) / BIRDS * 0x666666
+            );
+
+            (birdColors.array as Float32Array)[v * 3 + 0] = c.r;
+            (birdColors.array as Float32Array)[v * 3 + 1] = c.g;
+            (birdColors.array as Float32Array)[v * 3 + 2] = c.b;
+
+            (references.array as Float32Array)[v * 2] = x;
+            (references.array as Float32Array)[v * 2 + 1] = y;
+
+            (birdVertex.array as Float32Array)[v] = v % 9;
+
+        }
+
+        this.scale(0.2, 0.2, 0.2);
+
+    }
+
+}
+const birdVertexShader = `
+attribute vec2 reference;
+attribute float birdVertex;
+
+attribute vec3 birdColor;
+
+uniform sampler2D texturePosition;
+uniform sampler2D textureVelocity;
+
+varying vec4 vColor;
+varying float z;
+
+uniform float time;
+
+void main() {
+
+    vec4 tmpPos = texture2D( texturePosition, reference );
+    vec3 pos = tmpPos.xyz;
+    vec3 velocity = normalize(texture2D( textureVelocity, reference ).xyz);
+
+    vec3 newPosition = position;
+
+    newPosition = mat3( modelMatrix ) * newPosition;
+
+
+    velocity.z *= -1.;
+    float xz = length( velocity.xz );
+    float xyz = 1.;
+    float x = sqrt( 1. - velocity.y * velocity.y );
+
+    float cosry = velocity.x / xz;
+    float sinry = velocity.z / xz;
+
+    float cosrz = x / xyz;
+    float sinrz = velocity.y / xyz;
+
+    mat3 maty =  mat3(
+        cosry, 0, -sinry,
+        0    , 1, 0     ,
+        sinry, 0, cosry
+
+    );
+
+    mat3 matz =  mat3(
+        cosrz , sinrz, 0,
+        -sinrz, cosrz, 0,
+        0     , 0    , 1
+    );
+
+    newPosition =  maty * matz * newPosition;
+    newPosition += pos;
+
+    z = newPosition.z;
+
+    vColor = vec4( birdColor, 1.0 );
+    gl_Position = projectionMatrix *  viewMatrix  * vec4( newPosition, 1.0 );
+}
+`;
+const birdFragmentShader = `
+varying vec4 vColor;
+			varying float z;
+
+			uniform vec3 color;
+
+			void main() {
+				// Fake colors for now
+				float z2 = 0.2 + ( 1000. - z ) / 1000. * vColor.x;
+				gl_FragColor = vec4( z2, z2, z2, 1. );
+
+			}
+`;
+
+let birdUniforms;
+
+const birdGeometry = new BirdGeometry();
+
+// For Vertex and Fragment
+birdUniforms = {
+    'color': { value: new THREE.Color(0xff2200) },
+    'texturePosition': { value: null },
+    'textureVelocity': { value: null },
+    'time': { value: 1.0 },
+    'delta': { value: 0.0 }
+};
+
+// THREE.ShaderMaterial
+const birdMaterial = new THREE.ShaderMaterial({
+    uniforms: birdUniforms,
+    vertexShader: birdVertexShader,
+    fragmentShader: birdFragmentShader,
+    side: THREE.DoubleSide
+});
+
+const birdMesh = new THREE.Mesh(birdGeometry, birdMaterial);
+birdMesh.rotation.y = Math.PI / 2;
+birdMesh.matrixAutoUpdate = false;
+birdMesh.updateMatrix();
+
+scene.add(birdMesh);
+
+
+
 
 class PileEvent extends EventDispatcher {
     pile(pileX, pileZ) {
@@ -395,10 +590,11 @@ seasonListener.addEventListener('season_next', function (event) {
 var start = 0;
 function render() {
     const time = Date.now() * 0.001;
-    if (time - start >= 5) {
-        start = time;
-        seasonListener.next();
-    }
+    // if (time - start >= 5) {
+    //     start = time;
+    //     seasonListener.next();
+    // }
+    seasonListener.inform(SeasonType.Fall);
     if (summerFlag) {
         rainGroupSummer.children.forEach(sprite => {
             // 雨滴的y坐标每次减1
@@ -409,6 +605,8 @@ function render() {
             }
         });
     }
+
+    birdMaterial.uniformsNeedUpdate = true;
 
     if (springFlag) {
         leavesMaterial.uniforms.time.value = clock.getElapsedTime();
@@ -449,5 +647,7 @@ function fallShow() {
     console.log('fall');
 }
 
-// var controls = new CONTROL.OrbitControls(camera, renderer.domElement);//创建控件对象
-// controls.addEventListener('change', render);//监听鼠标、键盘事件
+
+
+var controls = new CONTROL.OrbitControls(camera, renderer.domElement);//创建控件对象
+controls.addEventListener('change', render);//监听鼠标、键盘事件
